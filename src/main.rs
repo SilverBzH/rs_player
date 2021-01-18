b5a768f78f86103d7c8938cfa7f260c82b3693ee
Charlie Boutier <charlie@tempow.com>
2021-01-18 19:43:06 +0100

    Adding tokio lib to do some async stuff. UI thread and stream thread are now two different thread. Need to implement channel to send data between them.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,89 @@
+//std
+use std::process;
+
+//UI include
+use std::io;
+use tui::Terminal;
+use tui::backend::TermionBackend;
+use termion::raw::IntoRawMode;
+use tui::widgets::{Block, Borders, Sparkline};
+use tui::layout::{Layout, Constraint, Direction};
+use tui::style::{Style, Color};
+
+// Stream include
 use analogic_player::input::Input;
 use analogic_player::output::Output;
 use analogic_player::StreamDevice;
 use ringbuf::RingBuffer;
 
-fn main() -> Result<(), anyhow::Error> {
-    let (input_device, output_device) = init_stream()?;
-    input_device.play()?;
-    output_device.play()?;
+//Tokio include
+// use tokio::sync::oneshot::{self, Sender, Receiver};
 
-    loop {}
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+
+    // let (tx, rx): (Sender<f32>, Receiver<f32>) = oneshot::channel();
+
+    let terminal_task = tokio::spawn(async move {
+        let stdout = io::stdout().into_raw_mode()
+            .expect("Error stdout");
+        let backend = TermionBackend::new(stdout);
+        let mut terminal = Terminal::new(backend)
+            .expect("Error creating a new terminal");
+        terminal.draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints(
+                    [
+                        Constraint::Percentage(10),
+                        Constraint::Percentage(10),
+                        Constraint::Percentage(10)
+                    ]
+                ).split(f.size());
+            let block = Block::default()
+                .title("Block")
+                .borders(Borders::ALL);
+            f.render_widget(block, chunks[0]);
+            
+            let sparkline_block = Block::default()
+                .title("Sparkline")
+                .borders(Borders::ALL);
+            let sparkline_style = Style::default()
+                .fg(Color::Blue)
+                .bg(Color::Black);
+            let sparkline = Sparkline::default()
+                .block(sparkline_block)
+                .data(&[0, 2, 3, 4, 1, 4, 10])
+                .max(5)
+                .style(sparkline_style);
+            f.render_widget(sparkline, chunks[1]);
+        })
+    });
+
+    let stream_task = tokio::spawn(async move {
+        //Handle Stream
+
+        let err_msg = |err, stream_io| {
+            eprintln!("Error playing {} stream: {}", stream_io, err);
+            process::exit(2);
+        };
+        let (input_device, output_device) = init_stream()
+            .unwrap_or_else(|err| {
+                eprintln!("Error initiating the stream: {}", err);
+                process::exit(2);
+            });
+        input_device.play()
+            .unwrap_or_else(|err| { err_msg(err, "input"); });
+        output_device.play()
+            .unwrap_or_else(|err| { err_msg(err, "output"); });
+        loop {}
+    });
+
+    terminal_task.await??;
+    stream_task.await?;
+
+    Ok(())
 }
 
 fn init_stream() -> Result<(Input, Output), anyhow::Error> {
@@ -20,8 +95,8 @@ fn init_stream() -> Result<(Input, Output), anyhow::Error> {
     let mut input_device = Input::new(&host)?;
     let mut output_device = Output::new(&host)?;
 
-    println!("{}", input_device);
-    println!("{}", output_device);
+    // println!("{}", input_device);
+    // println!("{}", output_device);
 
     let config = &input_device.stream_config;
 
